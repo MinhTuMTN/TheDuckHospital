faa439c6c10ba93828d0d74e77febf75fc09ebf1
DiDi0703 <110220867+DiDi0703@users.noreply.github.com>
2023-12-19 18:41:47 +0700

    Update HistoryPage.jsx

--- the-duck-hospital-ui/src/pages/customer/HistoryPage.jsx
+++ the-duck-hospital-ui/src/pages/customer/HistoryPage.jsx
@@ -2,19 +2,27 @@ import styled from "@emotion/styled";
 import {
   Box,
   Breadcrumbs,
+  Button,
   Grid,
   Typography,
   useMediaQuery,
 } from "@mui/material";
 import React from "react";
 import HistoryDetail from "../../components/Customer/History/HistoryDetail";
-
+import { useNavigate } from "react-router-dom";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 const CustomTextBreakcrumb = styled(Typography)(({ theme }) => ({
   fontSize: "16px",
   fontWeight: "500",
   color: theme.palette.oldPrimaryDarker.main,
 }));
-
+const CustomButton = styled(Button)(({ theme }) => ({
+  borderRadius: "10px",
+  padding: "8px 15px",
+  toUpperCase: "none",
+  fontSize: "14px !important",
+  textTransform: "none",
+}));
 const patientInfo = {
   patient: [
     {
@@ -29,6 +37,9 @@ const patientInfo = {
 
 function HistoryPage(props) {
   const isLgUp = useMediaQuery((theme) => theme.breakpoints.up("lg"));
+  const isMdUp = useMediaQuery((theme) => theme.breakpoints.up("md"));
+  const navigate = useNavigate();
+
   const breakcrumbs = [
     <CustomTextBreakcrumb key={1}>Trang chủ</CustomTextBreakcrumb>,
     <CustomTextBreakcrumb key={2}>Lịch sử khám bệnh</CustomTextBreakcrumb>,
@@ -55,6 +66,34 @@ function HistoryPage(props) {
           textAlign: "center",
         }}
       >
+        <Grid
+          item
+          xs={12}
+          style={{
+            paddingRight: isMdUp ? "20px" : "0",
+            textAlign: "left",
+            justifyContent: "space-between",
+          }}
+        >
+          <CustomButton
+            variant="text"
+            sx={{
+              "&:hover": {
+                backgroundColor: "	#ffffff",
+              },
+            }}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            <ArrowBackIcon
+              sx={{
+                marginRight: "5px",
+              }}
+            />
+            Quay lại
+          </CustomButton>
+        </Grid>
         <Grid item xs={12}>
           <HistoryDetail
             diagnostic={"Bình thường"}
