1f43b86b6181daba6a728c584661dce28b13e096
DiDi0703 <110220867+DiDi0703@users.noreply.github.com>
2024-07-18 00:00:35 +0700

    fix: avatar in cashier page

--- the-duck-hospital-ui/src/components/Cashier/CashierAvatar.jsx
+++ the-duck-hospital-ui/src/components/Cashier/CashierAvatar.jsx
@@ -1,9 +1,13 @@
-import { HomeOutlined, LogoutOutlined } from "@mui/icons-material";
+import { ExpandMore, HomeOutlined, LogoutOutlined } from "@mui/icons-material";
 import {
   Avatar,
+  Badge,
   Box,
+  Button,
+  CardMedia,
   Divider,
   Popover,
+  Stack,
   Typography,
   styled,
 } from "@mui/material";
@@ -21,10 +25,23 @@ const StyledBox = styled(Box)({
   cursor: "pointer",
 });
 
+const CustomButton = styled(Box)`
+  padding: 4px 18px !important;
+  direction: ltr;
+  justify-content: space-between;
+  align-items: center;
+  border-left: 1px solid #e0e0e0;
+  border-radius: 0;
+  &:hover {
+    outline: none;
+    cursor: pointer;
+  }
+`;
+
 const PopoverItem = memo(({ children, color, icon, onClick }) => {
   const Icon = icon;
   return (
-    <StyledBox display={"flex"} onClick={onClick}>
+    <StyledBox display={"flex"} onClick={onClick} paddingX={1}>
       <Icon
         sx={{
           fontSize: "20px",
@@ -54,30 +71,86 @@ function CashierAvatar() {
     setAnchorEl(null);
   };
 
-  const { fullName, setToken } = useAuth();
+  const { fullName, setToken, avatar } = useAuth();
   const navigate = useNavigate();
 
   return (
     <Box
       sx={{
-        flexBasis: "10%",
+        flexBasis: "15%",
         display: "flex",
         justifyContent: "flex-end",
       }}
     >
-      <Avatar
-        onClick={handleClick}
-        aria-describedby={id}
-        alt="Avatar"
-        src="https://res.cloudinary.com/dsmvlvfy5/image/upload/v1702377250/camel_ckn4py.png"
-        sx={{
-          width: 40,
-          height: 40,
-          cursor: "pointer",
-          boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.419)",
-          padding: 0.5,
-        }}
-      />
+      <Stack direction={"row"} alignItems={"center"} spacing={1}>
+        <CustomButton component={Button} onClick={handleClick} ref={anchorEl}>
+          <span style={{ position: "relative" }}>
+            <CardMedia
+              component="img"
+              src={
+                avatar ||
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyyiip4CAXLbtJL5s2tQ4PdZgvr6NZJJ55rDT3kfPU-hNjoQMm_XqMVqlozf4XhyGMY_o&usqp=CAU"
+              }
+              sx={{
+                width: "40px",
+                height: "40px",
+                marginRight: 1,
+                borderRadius: "50%",
+                padding: "1px",
+                border: "1px solid #1d7ed8",
+                boxShadow: "0px 0px 1px 0px #41adff",
+              }}
+            />
+            <Badge
+              badgeContent=" "
+              variant="dot"
+              style={{
+                backgroundColor: "#0bb240",
+                borderRadius: "50%",
+                height: "12px",
+                width: "12px",
+                position: "absolute",
+                top: "30px",
+                right: "10px",
+                border: "2px solid #ffffff",
+              }}
+            />
+          </span>
+
+          <Stack direction={"column"} alignItems={"flex-start"}>
+            <Typography
+              variant={"body1"}
+              style={{
+                textTransform: "none",
+                fontSize: "12px",
+                fontWeight: "500",
+              }}
+            >
+              {fullName}
+            </Typography>
+            <Stack direction={"row"} alignItems={"center"} spacing={0.5}>
+              <Typography
+                variant={"body2"}
+                style={{
+                  textTransform: "none",
+                  fontSize: "10px",
+                  color: "#8c8c8c",
+                }}
+              >
+                Thu ngân
+              </Typography>
+              <ExpandMore
+                sx={{
+                  fontSize: "14px",
+                  color: "#8c8c8c",
+                  marginTop: "-2px",
+                }}
+              />
+            </Stack>
+          </Stack>
+        </CustomButton>
+      </Stack>
+
       <Popover
         id={id}
         open={open}
@@ -88,12 +161,33 @@ function CashierAvatar() {
           horizontal: "left",
         }}
       >
-        <Box
+        <Stack
+          spacing={1}
+          direction={"column"}
           sx={{
-            padding: "12px",
+            px: "8px",
+            paddingY: "16px",
+            width: "235px",
           }}
         >
-          <Typography fontWeight={500}>Xin chào, {fullName}</Typography>
+          <Stack direction={"row"} spacing={1.7} paddingX={1}>
+            <Avatar />
+            <Stack direction={"column"}>
+              <Typography
+                variant="h6"
+                style={{ fontSize: "14px", fontWeight: "400" }}
+              >
+                Xin chào,
+              </Typography>
+              <Typography
+                variant="h6"
+                sx={{ fontSize: "14px", fontWeight: "bold" }}
+              >
+                {fullName}
+              </Typography>
+            </Stack>
+          </Stack>
+
           <Divider />
           <PopoverItem
             color={appColors.primary}
@@ -115,7 +209,7 @@ function CashierAvatar() {
           >
             Đăng xuất
           </PopoverItem>
-        </Box>
+        </Stack>
       </Popover>
     </Box>
   );
