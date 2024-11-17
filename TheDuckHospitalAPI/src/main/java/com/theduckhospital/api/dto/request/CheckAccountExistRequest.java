3d36b1e9673eb6f738c89cc59672ef5c49cd4a70
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2024-11-17 12:31:05 +0700

    Fix bug: Validate phone number

--- TheDuckHospitalAPI/src/main/java/com/theduckhospital/api/dto/request/CheckAccountExistRequest.java
+++ TheDuckHospitalAPI/src/main/java/com/theduckhospital/api/dto/request/CheckAccountExistRequest.java
@@ -1,8 +1,10 @@
 package com.theduckhospital.api.dto.request;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class CheckAccountExistRequest {
+    @Pattern(regexp = "^(0\\d{9}|.*@.*)$", message = "Invalid email or phone number")
     public String emailOrPhoneNumber;
 }
